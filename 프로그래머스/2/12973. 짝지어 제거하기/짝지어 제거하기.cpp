055d4b8ba3b5b575642bfd1dbe270ba7a7180938
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2024-06-16 20:38:09 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.11 ms, Memory: 6.41 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,26 +1,21 @@
 #include <iostream>
-#include<string>
-#include<stack>
+#include <string>
+#include <stack>
 using namespace std;
 
 int solution(string s)
 {
-    int answer = 0;
-    if(s.empty() || s.size() % 2 != 0)
-        return answer;
-
-    stack<char> stack;
-    for(size_t i = 0; i < s.length(); i++)
-    {
-        if(stack.empty() || stack.top() != s[i])
-            stack.push(s[i]);
-        else
-            stack.pop();
-        
+    stack<char> st;
+    st.push(s[0]);
+    for(int i = 1; i < s.size(); i++){
+        if(!st.empty()){
+            if(st.top() == s[i]){
+                st.pop();
+                continue;
+            }
+        }
+        st.push(s[i]);
     }
-
-    if(stack.empty())
-        answer++;
     
-    return answer;
+    return st.empty();
 }
