bcb2f37642a7d2ee31223430d86c770e29475960
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2024-06-27 14:28:01 +0900

    [level 2] Title: 귤 고르기, Time: 14.19 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.cpp
@@ -1,30 +1,29 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 #include <unordered_map>
+#include <algorithm>
 using namespace std;
+
+bool compare(const pair<int, int>& a, const pair<int, int>& b) {
+    return a.second > b.second;
+}
 
 int solution(int k, vector<int> tangerine) {
     int answer = 0;
-    unordered_map<int, int> m;
-    vector<int> v;
-    int n = 0;
+    unordered_map<int, int> um;
+    
     for(int i = 0; i < tangerine.size(); i++){
-        m[tangerine[i]]++;
+        um[tangerine[i]]++;    
     }
     
-    for(auto a : m){
-        v.push_back(a.second);
-    }
-   
-    sort(v.begin(), v.end(), greater<>());
+    vector<pair<int,int>> v(um.begin(), um.end());
+    sort(v.begin(), v.end(), compare);
     
-    for(auto a : v){
-        if(n >= k){
-            break;
-        }
-        n += a;
+    for (const auto& pair : v) {
+        k -= pair.second;
         answer++;
+        if(k <= 0)
+            break;
     }
     
     return answer;
