73dcb6778db52bbb370674f7ba8d6ef59c669c53
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2025-08-25 15:04:24 +0900

    [level 2] Title: 주식가격, Time: 27.62 ms, Memory: 26.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,22 +1,19 @@
 #include <string>
 #include <vector>
+#include <queue>
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
     vector<int> answer;
-    int startIndex = 0;
-    
-    while(startIndex != prices.size() - 1){
-        int count = 0;
-        for(int i = startIndex + 1; i < prices.size(); i++){
-            count++;
-            if(prices[startIndex] > prices[i] || 
-              i == prices.size() - 1){
-                answer.push_back(count);
+    for(int i = 0; i < prices.size() - 1; i++){
+        int c = 0;
+        for(int j = i + 1; j < prices.size(); j++){
+            c++;
+            if(prices[i] > prices[j]){
                 break;
             }
         }
-        startIndex++;
+        answer.push_back(c);
     }
     
     answer.push_back(0);
