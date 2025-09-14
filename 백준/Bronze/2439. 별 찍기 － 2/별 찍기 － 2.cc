de2bb731264856dfa8db2c6fd7e0d1206f71a2cf
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2025-09-15 02:28:51 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = N; i > 0; i--) {
+        for (int j = 1; j <= N; j++) {
+            if (i <= j){
+                cout << "*";
+            }
+            else {
+                cout << " ";
+            }
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
