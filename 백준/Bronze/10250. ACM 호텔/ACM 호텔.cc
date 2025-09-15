63d8cbac4698291e339c4564c2874db1bb6c2ed0
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2025-09-15 14:30:18 +0900

    [Bronze III] Title: ACM 호텔, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    int H, W, N;
+	int result;
+    cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> H >> W >> N;
+
+		if (N % H == 0) {
+			result = H * 100 + (N / H);
+		}
+		else {
+			result = (N % H) * 100 + (N / H) + 1;
+		}
+		cout << result << "\n";
+	}
+
+    return 0;
+}
