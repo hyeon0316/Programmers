825120c1b4bd9817465a1dee449ce8346734b3f5
hyeon0316 <92031289+hyeon0316@users.noreply.github.com>
2025-12-25 12:45:03 +0900

    [Bronze II] Title: Strfry, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11328. Strfry/Strfry.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        string a, b;
+        cin >> a >> b;
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        if (a == b) {
+            cout << "Possible" << "\n";
+        }
+        else {
+            cout << "Impossible" << "\n";
+        }
+    }
+
+}
